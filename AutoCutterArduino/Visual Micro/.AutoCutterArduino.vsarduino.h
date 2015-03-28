45806ed705c4615a09abaf8b9f7da04ffae415a2
AnishKrishnan <anish.krishnan2708@gmail.com>
2015-03-28 23:46:04 +1300

    Revert "Created CommsLinkFactory"
    This reverts commit f9b786fe2d1169bd8daff397cc7a4a5d4ac4707f.

--- AutoCutterArduino/Visual Micro/.AutoCutterArduino.vsarduino.h
+++ AutoCutterArduino/Visual Micro/.AutoCutterArduino.vsarduino.h
@@ -11,6 +11,7 @@
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
 #define __AVR_ATmega2560__
+#define _VMDEBUG 1
 #define ARDUINO 105
 #define ARDUINO_MAIN
 #define __AVR__
@@ -66,8 +67,6 @@ extern "C" void __cxa_pure_virtual() {;}
 #include <CommsController.h>
 #include <CommsLinkBase.cpp>
 #include <CommsLinkBase.h>
-#include <CommsLinkFactory.cpp>
-#include <CommsLinkFactory.h>
 #include <Config.h>
 #include <DebugPortFactory.cpp>
 #include <DebugPortFactory.h>
